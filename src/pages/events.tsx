38ef45e7fbc5f28186201729b0e781db777ab747
Jonathan Reyes <jer88@duke.edu>
2024-09-26 17:35:25 +0900

    events graphics

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -4,14 +4,12 @@ import EventsList from 'src/templates/EventList';
 
 import { EventHero } from '../templates/eventsHero';
 import { Footer } from '../templates/Footer';
-import EventScheduler from '../templates/Scheduler';
 
 const Events: React.FC = () => {
   return (
     <div>
       <EventHero />
       <EventsList />
-      <EventScheduler />
       <Footer />
     </div>
   );
