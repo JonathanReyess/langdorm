e3b70f6ddb15d52a615e29f097a80f67a605e90b
Jonathan Reyes <jer88@duke.edu>
2024-11-05 01:30:31 +0900

    aesthetic margins

--- src/components/navigation/NavbarTwoColumns.tsx
+++ src/components/navigation/NavbarTwoColumns.tsx
@@ -21,7 +21,9 @@ const NavbarTwoColumns = (props: INavbarProps) => {
       </div>
 
       {/* Mobile Menu Toggle (Hamburger Icon) */}
-      <div className="sm:hidden mr-4"> {/* Added mr-4 for right margin */}
+      <div className="mr-4 sm:hidden">
+        {' '}
+        {/* Added mr-4 for right margin */}
         <button
           onClick={() => setIsMenuOpen(!isMenuOpen)}
           className="text-gray-800 hover:text-primary-500 focus:outline-none"
